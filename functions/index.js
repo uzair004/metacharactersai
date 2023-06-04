899bba09f058c6340e3ce8adf148e50c7e1572a5
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2023-06-05 02:26:40 +0500

    Add basic code

---
+++ functions/index.js
@@ -0,0 +1,40 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+// Initialize the Firebase Admin SDK with a service account
+const serviceAccount = require('../firebase-credentials.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+2
+exports.processNewMessage = functions.firestore
+  .document('Conversations/{conversationId}/Messages/{messageId}')
+  .onCreate((snapshot, context) => {
+    const newMessageData = snapshot.data();
+    const isSystemGenerated = newMessageData.isSystemGenerated || false;
+
+    // Example:
+    console.log('New message:', newMessageData);
+
+    if (isSystemGenerated) {
+      // This message is system-generated, no further processing needed
+      return null;
+    }
+
+    // Handle the logic for processing the new message here
+    // For example, call the ChatGPT API and update the document with the generated response
+    // You can access the new message data using snapshot.data()
+    // You can access the conversation and message IDs using context.params
+
+    // Set a flag indicating that the next message will be system-generated
+    const nextMessageData = {
+      isSystemGenerated: true
+    };
+
+    // Generate the response using the ChatGPT API
+    // Insert the response as a new message in the Messages subcollection
+    const conversationId = context.params.conversationId;
+    const messagesRef = admin.firestore().collection('Conversations').doc(conversationId).collection('Messages');
+    
+    return messagesRef.add(nextMessageData);
+  });
---
+++ models/index.js
@@ -0,0 +1,47 @@
+// models.js
+
+// User schema
+class User {
+    constructor(userId, username, email) {
+      this.userId = userId;
+      this.username = username;
+      this.email = email;
+    }
+  }
+  
+  // Model schema
+  class Model {
+    constructor(modelId, name, prompt, contextSize) {
+      this.modelId = modelId;
+      this.name = name;
+      this.prompt = prompt;
+      this.contextSize = contextSize;
+    }
+  }
+  
+  // Conversation schema
+  class Conversation {
+    constructor(conversationId, userId, modelId) {
+      this.conversationId = conversationId;
+      this.userId = userId;
+      this.modelId = modelId;
+    }
+  }
+  
+  // Message schema
+  class Message {
+    constructor(messageId, role, text, timestamp) {
+      this.messageId = messageId;
+      this.role = role;
+      this.text = text;
+      this.timestamp = timestamp;
+    }
+  }
+  
+  module.exports = {
+    User,
+    Model,
+    Conversation,
+    Message,
+  };
+  
