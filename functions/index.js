21785716142daed5b30093117161cb0ed6f6b536
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2023-06-19 01:55:38 +0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,65 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const { v1: uuid, } = require('uuid');
+const { generateChatReply } = require('./services/openai')
 
 // Initialize the Firebase Admin SDK with a service account
-const serviceAccount = require('../firebase-credentials.json');
+const serviceAccount = require('./firebase-credentials.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
 exports.processNewMessage = functions.firestore
   .document('Conversations/{conversationId}/Messages/{messageId}')
-  .onCreate((snapshot, context) => {
+  .onCreate(async (snapshot, context) => {
     const newMessageData = snapshot.data();
     const isSystemGenerated = newMessageData.isSystemGenerated || false;
-
-    // Example:
-    console.log('New message:', newMessageData)
-    console.log('context: ', context)
 
     if (isSystemGenerated) {
       // This message is system-generated, no further processing needed
       return null;
     }
 
-    // For example, call the ChatGPT API and update the document with the generated response
-    // You can access the conversation and message IDs using context.params
+    if(!newMessageData.modelId) {
+      console.error('modelId not found in inserted message')
+      return null
+    }
+
+    if(!newMessageData.content) {
+      console.error('content missing in message')
+      return null
+    }
+
+    if(!newMessageData.messageId) {
+      console.error('messageId missing in message')
+      return null
+    }
+
+    // Fetch the modelId from the new message data
+    const modelId = newMessageData.modelId;
+
+    // Retrieve the model reference from the Models collection using modelId
+    const modelRef = admin.firestore().collection('Models').doc(modelId);
+    const modelDoc = await modelRef.get();
+
+    if (!modelDoc.exists) {
+      // Model document not found, handle the error or return
+      console.error(`Model document with id ${modelId} does not exist.`);
+      return null;
+    }
+
+    // Get the prompt attribute value from the model document
+    const systemPrompt = modelDoc.data().prompt;
+
+    const reply = await generateChatReply(newMessageData.content, [], systemPrompt)
 
     // Set a flag indicating that the next message will be system-generated
     const nextMessageData = {
       isSystemGenerated: true,
       messageId: uuid(),
       role: "assistant",
-      content: 'I am elon musk',
-      timestamp: admin.firestore.FieldValue.serverTimestamp(),
+      content: reply,
+      timestamp: new Date().getTime(),
       replyTo: newMessageData.messageId 
     };
 
