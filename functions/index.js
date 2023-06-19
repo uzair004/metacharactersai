7038fa5202da7b6958a6910022ea3cb79cb07226
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2023-06-20 02:22:10 +0500

    Update cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const { v1: uuid, } = require('uuid');
-const { generateChatReply } = require('./services/openai')
+const { v1: uuid } = require('uuid');
+const { generateChatReply } = require('./services/openai');
 
 // Initialize the Firebase Admin SDK with a service account
 const serviceAccount = require('./firebase-credentials.json');
@@ -14,76 +14,87 @@ exports.processNewMessage = functions.firestore
   .onCreate(async (snapshot, context) => {
     const newMessageData = snapshot.data();
 
-    const { isSystemGenerated, modelId, content, messageId } = newMessageData
-    const { conversationId } = context.params
+    const { isSystemGenerated, modelId, content, messageId } = newMessageData;
+    const { conversationId } = context.params;
 
     if (isSystemGenerated) {
       // This message is system-generated, no further processing needed
       return null;
     }
 
-    if(!modelId) {
-      console.error('modelId not found in inserted message')
-      return null
+    if (!modelId) {
+      console.error('modelId not found in inserted message');
+      return null;
     }
 
-    if(!content) {
-      console.error('content missing in message')
-      return null
+    if (!content) {
+      console.error('content missing in message');
+      return null;
     }
 
-    if(!messageId) {
-      console.error('messageId missing in message')
-      return null
+    if (!messageId) {
+      console.error('messageId missing in message');
+      return null;
     }
-
 
     // Retrieve the model reference from the Models collection using modelId
-    const modelRef = admin.firestore().collection('Models').doc(modelId);
-    const modelDoc = await modelRef.get();
+    const modelQuerySnapshot = await admin.firestore()
+      .collection('Models')
+      .where('modelId', '==', modelId)
+      .limit(1)
+      .get();
 
-    if (!modelDoc.exists) {
+    if (modelQuerySnapshot.empty) {
       // Model document not found, handle the error or return
       console.error(`Model document with id ${modelId} does not exist.`);
       return null;
     }
 
+    const modelDoc = modelQuerySnapshot.docs[0];
+
     // Get the prompt attribute value from the model document
     const systemPrompt = modelDoc.data().prompt;
 
     // Retrieve the latest 5 messages from the Messages subcollection of the same conversation
-    const messagesRef = admin.firestore().collection('Conversations').doc(conversationId).collection('Messages');
-    const querySnapshot = await messagesRef.orderBy('timestamp', 'desc').limit(5).get();
+    const messagesRef = admin.firestore()
+      .collection('Conversations')
+      .doc(conversationId)
+      .collection('Messages');
 
-    let previousConversation = []
-    
+    const querySnapshot = await messagesRef
+      .orderBy('timestamp', 'desc')
+      .limit(5)
+      .get();
+
+    const previousConversation = [];
+
     // Extract the role and content attributes from the messages
-    querySnapshot.docs.forEach((doc) => {
-      const messageData = doc.data();
+    querySnapshot.forEach((doc) => {
+      const { role, content } = doc.data();
 
-      const { role, content } = messageData
-
-        if(role && content && role !== 'system') {
-          previousConversation.push({
-            role,
-            content
-          })
-        }
+      if (role && content && role !== 'system') {
+        previousConversation.push({
+          role,
+          content
+        });
+      }
     });
 
-    const reply = await generateChatReply(content, previousConversation, systemPrompt)
+    const reply = await generateChatReply(content, previousConversation, systemPrompt);
+
+    console.log({ reply });
 
     // Set a flag indicating that the next message will be system-generated
     const nextMessageData = {
       isSystemGenerated: true,
       messageId: uuid(),
-      role: "assistant",
+      role: 'assistant',
       content: reply,
       timestamp: new Date().getTime(),
       replyTo: messageId,
       modelId: modelId
     };
 
-    // Insert the response as a new message in the Messages subcollection    
+    // Insert the response as a new message in the Messages subcollection
     return messagesRef.add(nextMessageData);
   });
