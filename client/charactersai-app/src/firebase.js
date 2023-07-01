762833f8ca8c0855d8d27233c7e806c22b3fe764
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2023-07-01 23:33:02 +0500

    Import & export auth

--- client/charactersai-app/src/firebase.js
+++ client/charactersai-app/src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import'firebase/compat/storage';
+import 'firebase/compat/auth';
 
 
 const firebaseConfig = {
@@ -17,5 +18,6 @@ firebase.initializeApp(firebaseConfig);
 
 export const firestore = firebase.firestore();
 export const storage = firebase.storage();
+export const auth = firebase.auth();
 
 
